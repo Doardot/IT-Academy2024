4f695a1b8f428c4c33e78314fbedd308140eba49
Eduardo Tavares <eduardo.tavares002@edu.pucrs.br>
2024-03-20 09:08:57 -0300

    Remove usuariosDeTeste from LotteryUserInterface

--- src/aplication/LotteryUserInterface.java
+++ src/aplication/LotteryUserInterface.java
@@ -21,19 +21,7 @@ public class LotteryUserInterface {
         this.lotteryRunner = new LotteryRunner(betRegistry, betCreator);
         this.awards = new Awards();
 
-        usuariosDeTeste();
         menu();
-    }
-
-    public void usuariosDeTeste() {
-        HashSet<Integer> numerosApostadosMais5 = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
-                33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50));
-        Bet a = new Bet("Poggers", 123456789, 1, numerosApostadosMais5);
-        Bet b = new Bet("Ana", 987654321, 2, numerosApostadosMais5);
-
-        betRegistry.addBets(a);
-        betRegistry.addBets(b);
     }
 
     public void menu() {
